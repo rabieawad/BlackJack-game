ccb1e90377201989847ae9c6f6e9c5a96683143a
Rabie Alawad <101249713+rabieawad@users.noreply.github.com>
2023-06-18 15:21:25 +0200

    Add files via upload

---
+++ Client/src/tests/InputHandlerTest.java
@@ -0,0 +1,13 @@
+package org.boes.praktikum.gameclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputHandlerTest {
+
+    @Test
+    void startGame() {
+        assertEquals(1,1);
+    }
+}
